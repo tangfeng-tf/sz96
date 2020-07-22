f03a809738e89f19e1282241f2b6089a76ce0f2c
汤峰 <1024601656@qq.com>
2020-07-22 11:51:00 +0800

    修改了

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-	public static void mian(String[] arhs) {
+	public static void mian(String[] args) {
 		System.out.prinln("hello world");
+		System.out.prinln("ÄãºÃjava");
 	}
 }
